be20e32f63c12d0f5c8bae91a5c232805e664a69
XiaoYing <2944418159@qq.com>
2020-06-10 21:58:59 +0800

    完成了用户修改的功能

---
+++ assets/js/user/changeavatar.js
@@ -0,0 +1,78 @@
+$(function () {
+    // 1.1 获取裁剪区域的 DOM 元素
+    var $image = $('#image');
+
+    // 1.2 配置选项     const定义的变量是不可以修改的，而且必须初始化
+    const options = {
+        // 纵横比   剪裁框的宽高比
+        aspectRatio: 1,
+        // 指定预览区域   是一个dom元素，必须通过querySelect可以获取到
+        preview: '.img-preview'
+    }
+
+    // 1.3 创建裁剪区域
+    $image.cropper(options);
+
+    //-------------点击上传按钮，上传图片-----------
+    /*
+       在js中，只有一种类型的可以上传文件，那就是input-file，可以通过点击上传事件按钮触发input这个按钮的事件
+    */
+    $('.goto').on('click', function () {
+
+        $('input[type=file]').click();
+
+
+    })
+
+
+
+    //---------------有问题----------------
+
+    //----------上传图片之后，获取url地址，改变剪裁框中的图片-----------
+    $('input[type=file]').on('change', function () {
+        // 获取上传的文件对象
+
+        var file = this.files[0];
+
+        // 得到上传文件对象的url地址
+        var url = URL.createObjectURL(file);
+
+        // 将图片显示在剪裁框中，剪裁框一旦确定了图片就无法改变，只有销毁了之后在重新创建才可以
+        $image.cropper('destroy').attr('src', url).cropper(options);
+    })
+
+    //----------点击确定按钮，裁剪图片，提交ajax，改变头像----------
+    $('.sure').on('click', function () {
+        var dataURL = $image
+            .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+                width: 100,
+                height: 100
+            })
+            .toDataURL('image/.png')       // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+
+        $.ajax({
+            type: 'post',
+            url: 'http://www.liulongbin.top:3007/my/update/avatar',
+            data: {
+                avatar: dataURL
+            },
+            success: function (res) {
+                layer.msg(res.message);
+                if (res.status == 0) {
+                    // 更换成功，刷新页面，父页面的
+                    window.parent.rander();
+                }
+            },
+            headers: {
+                'Authorization': localStorage.getItem('token')
+            },
+            complete: function (xhr) {
+                if (xhr.responseJSON.status == 1 && xhr.responseJSON.message === '身份认证失败！') {
+                    localStorage.responseJSON('token');
+                    window.parent.location.href = '/login.html'
+                }
+
+            }
+        })
+    })
+})
--- assets/js/user/userinfo.js
+++ assets/js/user/userinfo.js
@@ -7,9 +7,11 @@ function renderUser() {
         success: function (res) {
             if (res.status == 0) {
                 //实现数据回显
-                $('form input[name=username]').val(res.data.username);
-                $('form input[name=nickname]').val(res.data.nickname);
-                $('form input[name=email]').val(res.data.email);
+                // $('form input[name=username]').val(res.data.username);
+                // $('form input[name=nickname]').val(res.data.nickname);
+                // $('form input[name=email]').val(res.data.email);
+                // 使用layui的的表单赋值方法，前提是表单有lay-filter=""值
+                form.val('abc', res.data);
             }
         },
         headers: {
@@ -28,5 +30,46 @@ function renderUser() {
 }
 
 $(function () {
+    //-------------数据回显---------------
     renderUser();
+
+    //-----------点击修改按钮----------
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'post',
+            url: 'http://www.liulongbin.top:3007/my/userinfo',
+            // 不获取被禁用的属性值
+            data: $(this).serialize(),
+
+            success: function (res) {
+                layer.msg(res.message);
+                if (res.status == 0) {
+                    //修改成功之后要重新加载index页面，让更改的内容显示在侧栏中，之前加载页面的被封装成一个函数，在index页面中，也就是当前页面的父页面
+                    window.parent.rander();
+                }
+            },
+            headers: {
+                'Authorization': localStorage.getItem('token')
+            },
+            complete: function (xhr) {
+                if (xhr.responseJSON.status == 1 && xhr.responseJSON.message === '身份认证失败！') {
+                    localStorage.removeItem('token');
+                    window.parent.location.href = '/login.html';
+                }
+
+            }
+        })
+
+    })
+
+
+    //-----------点击重置按钮----------------
+    $('button[type=reset]').on('click', function (e) {
+        //重置按钮的默认行为就是清空所有数据，所以要阻止
+        e.preventDefault();
+        // 此处的重置是回复到之前的页面，相当于用户信息重新回显
+        renderUser();
+
+    })
 })
